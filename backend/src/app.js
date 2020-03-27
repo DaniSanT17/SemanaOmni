fbdec03ecfd50bc4700f09ea1e46cdf8730cb235
unknown <dan-santiago@live.com>
2020-03-27 16:53:06 -0400

    test-config

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes')
+const { errors } = require('celebrate');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 app.use(routes);
+app.use(errors());
+
+module.exports = app;
+
 /*
 Rotas e recursos
 */
@@ -35,4 +40,3 @@ Query Builder: table('users').select('*').where()
 
 
 
-app.listen(3333);
